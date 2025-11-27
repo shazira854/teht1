db04b30300be08e28f5a9f9ad65f99d068b79e64
shazira854 <p74515@edu.sasky.fi>
2025-11-27 12:40:26 +0200

    osa koodista
    tehty osa koodista

--- Program.cs
+++ Program.cs
@@ -1,30 +1,81 @@
-﻿// Esimerkkiratkaisu tehtävää 1
-// Pirkko Sutinen 13.4.2021
-// pirkko.sutinen@sasky.fi
+﻿using System;
+using System.Collections.Generic;
 
-using System;
-
-namespace tehtävä1
+namespace Kotikirjasto
 {
+
+    class Book
+    {
+        public string Name;
+        public string Author;
+        public int Year;
+        public string Genre;
+    }
+
+
     class Program
     {
+
+        static List<Book> books = new List<Book>();
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Mikä sinun nimesi on?");
-            string nimi = Console.ReadLine();
-            Console.WriteLine("Minä ikäinen olet?");
-            string ika = Console.ReadLine();
-            bool tulos;
-            int iika;
-            tulos = int.TryParse(ika, out ika);
-            if(tulos)
+
+            bool run = true;
+
+
+            while (run)
             {
-                Console.WriteLine($"Ajattele {nimi.ToUpper()}, vuonna 2031 olet jo {ika + 10} vuotta vanha");
+                Console.WriteLine("Kotikirjasto");
+                Console.WriteLine("1. Lisaa kirja");   // teen nää viel
+                Console.WriteLine("2. Nayta kirjat");
+                Console.WriteLine("3. Lopeta");
+                Console.Write("Valitse: ");
+
+                string choice = Console.ReadLine();
+
+
+                if (choice == "1")
+                {
+                    AddBook();   // ei valmis viel
+                }
+                else if (choice == "2")
+                {
+                    ShowBooks();  // ei valmis
+                }
+                else if (choice == "3")
+                {
+                    run = false;
+                }
+                else
+                {
+                    Console.WriteLine("Virhe valinnassa");
+                }
+
+                Console.WriteLine(""); 
             }
-            else
-            {
-                Console.WriteLine("Anna ikä numerona");
-            }
+
         }
+
+
+
+        static void AddBook()
+        {
+            // teen taman myohemmin
+            Console.WriteLine("Tama ei toimi viela...");
+        }
+
+
+
+        static void ShowBooks()
+        {
+            // ei toimi viela
+            Console.WriteLine("Kirjoja ei nayteta viela...");
+        }
+
+
     }
+
 }
+
